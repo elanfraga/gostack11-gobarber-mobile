01cf44c57ff87ba9a4762a2c5dba70cbe137db6c
Elan Fraga <elanfraga@gmail.com>
2020-06-11 22:07:48 -0300

    Estrutura da criação

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatr,
+} from './styled';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+  const { goBack } = useNavigation();
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={goBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatr source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -12,6 +12,7 @@ export const Header = styled.View`
   padding: 24px;
   /* padding-top: ${getStatusBarHeight() + 24}px; */
   background: #28262e;
+
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
