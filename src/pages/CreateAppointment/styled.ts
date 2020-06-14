e5b67ce31ae1ccbe29c4993253eed264a7435a8b
Elan Fraga <elanfraga@gmail.com>
2020-06-13 21:09:10 -0300

    Mostrando horários em tela

--- src/pages/CreateAppointment/styled.ts
+++ src/pages/CreateAppointment/styled.ts
@@ -13,6 +13,15 @@ interface ProviderNameProps {
   selected: boolean;
 }
 
+interface HourProps {
+  available: boolean;
+  selected: boolean;
+}
+
+interface HourTexProps {
+  selected: boolean;
+}
+
 export const Container = styled.View`
   flex: 1;
 `;
@@ -43,7 +52,9 @@ export const UserAvatr = styled.Image`
   margin-left: auto;
 `;
 
-export const ProviderListContainer = styled.View`
+export const Content = styled.View``;
+
+export const ProviderListContainer = styled.ScrollView`
   height: 112px;
 `;
 
@@ -95,4 +106,40 @@ export const OpenDatePickerButtonText = styled.Text`
   font-family: 'RobotoSlab-Medium';
   font-size: 16px;
   color: #232129;
+`;
+
+export const Schedule = styled.View`
+  padding: 24px 0 16px;
+`;
+
+export const Section = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const SectionTitle = styled.Text`
+  font-size: 18px;
+  color: #999591;
+  font-family: 'RobotoSlab-Medium';
+  margin: 0 24px 12px;
+`;
+
+export const SectionContent = styled.ScrollView.attrs({
+  contentContainerStyle: { paddingHorizontal: 24 },
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const Hour = styled(RectButton)<HourProps>`
+  padding: 12px;
+  background: ${props => (props.selected ? '#ff9000' : '#3e3b47')};
+  border-radius: 10px;
+  margin-right: 8px;
+
+  opacity: ${props => (props.available ? 1 : 0.3)};
+`;
+
+export const HourText = styled.Text<HourTexProps>`
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
 `;
